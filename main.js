568af798f688de282b53d0dc1b3eff15073e9583
Tomas <tomas.a.gallardo006@gmail.com>
2025-11-06 16:25:05 -0300

    git

--- main.js
+++ main.js
@@ -1,35 +1,24 @@
 const monto=850000
 
-
-
 const tasaPromedio = (anio1,anio2,anio3) =>{
-    tasapromedio= (anio1 + anio2+ anio3)/3
-    return tasapromedio
+    return promedio= (anio1 + anio2+ anio3)/3
+
 }
 
 const inversionAnioCompleto = (tasapromedio,monto) =>{
-    montoFinal=((tasapromedio/100) + 1 ) * monto  
-    return montoFinal
+   return montoFinal=((tasapromedio/100) + 1 ) * monto
 }
 
-const inversionPorTrimestre = (tasaTrimestre,monto) =>{
-    
-
-    montoTrimestre=((tasaTrimestre/100) + 1 ) * monto  
-
-
-
-    return montoTrimestre
+const inversionPorTasa = (tasaTrimestre,monto) =>{
+    return  montoTrimestre=((tasaTrimestre/100) + 1 ) * monto
 }
-
-
 const inversionTotalCuatrimestre = (tasapromedio) =>{
-    tasaTrimestre=tasapromedio/4;
+    const tasaTrimestre=tasapromedio/4;
     let contador=0
 
-    inversionTotalFinal= 850000
-    while (contador<3){
-       const inversion= inversionPorTrimestre (tasaTrimestre,inversionTotalFinal)
+    inversionTotalFinal= monto
+    while (contador<4){
+       const inversion= inversionPorTasa (tasaTrimestre,inversionTotalFinal)
 
        inversionTotalFinal= inversion
 
@@ -38,3 +27,27 @@ const inversionTotalCuatrimestre = (tasapromedio) =>{
 
     return inversionTotalFinal
 }
+const inversionTotalPorMes = (tasapromedio) =>{
+    const tasaTrimestre=tasapromedio/12;
+    let contador=0
+
+    inversionTotalFinal= monto
+    while (contador<12){
+       const inversion= inversionPorTasa (tasaTrimestre,inversionTotalFinal)
+
+       inversionTotalFinal= inversion
+
+        contador++
+    }
+
+    return inversionTotalFinal
+}
+
+const Banco1 = document.getElementById("banco1")
+const Banco2 = document.getElementById("banco2")
+const Banco3 = document.getElementById("banco3")
+let boton = document.getElementById("boton")
+
+boton.addEventListener('click', function (){
+ 
+})
